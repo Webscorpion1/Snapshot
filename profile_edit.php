77f94b0f2cfaf9f633e689930eb1d4fcf2e20417
Lucas Debelder <lucasdebelder@hotmail.com>
2018-04-29 19:00:41 +0200

    fixed nav highlight issue

--- profile_edit.php
+++ profile_edit.php
@@ -67,7 +67,20 @@ if (in_array($fileActualExt, $allowed)) {
     <meta name="twitter:image" content="">
 
 </head>
-<?php include_once("includes/nav.inc.php"); ?>
+<nav>
+    <ul>
+
+        <li><img src="media/frontend/logo.svg" alt="Logo" ></li>
+        <form class="nav_search" action="" method="post">
+            <input type="text" name="search" id="search" placeholder="&#xF002; Search on tags" style="font-family:Arial, FontAwesome" />
+        </form>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="addpost.php">Add post</a></li>
+        <li><a class="active" href="profile.php?user=<?php echo $_SESSION['userid']; ?>">Profile</a></li>
+        <li><a href="logout.php">Log out</a></li>
+
+    </ul>
+</nav>
 <form action="" method="post" enctype="multipart/form-data">
     <h1 form__title>Update account</h1>
 
