720e72af3ffc67739355ff0cc7963deab2a76b73
Lucas Debelder <lucasdebelder@hotmail.com>
2018-05-13 13:00:52 +0200

    CSS float removed

--- index.php
+++ index.php
@@ -146,15 +146,21 @@ function colorPalette($imageFile, $numColors, $granularity = 5)
 <body>
 <nav>
     <ul>
-        <li><img src="media/frontend/logo.svg" alt="Logo" ></li>
+        <div class="logo">
+            <li><img src="media/frontend/logo.svg" alt="Logo" ></li>
+        </div>
+
         <form class="nav_search" action="" method="post">
             <input class="form__input" type="text" name="search" id="search" placeholder="Search on tags" style="font-family:Arial, FontAwesome" />
         </form>
-        <li><a class="active" href="index.php">Home</a></li>
-        <li><a href="friends.php">Friend's posts</a></li>
-        <li><a href="addpost.php">Add post</a></li>
-        <li><a href="profile.php?user=<?php echo $_SESSION['userid']; ?>">Profile</a></li>
-        <li><a href="logout.php">Log out</a></li>
+
+        <div class="links">
+            <li><a class="active" href="index.php">Home</a></li>
+            <li><a href="friends.php">Friend's posts</a></li>
+            <li><a href="addpost.php">Add post</a></li>
+            <li><a href="profile.php?user=<?php echo $_SESSION['userid']; ?>">Profile</a></li>
+            <li><a href="logout.php">Log out</a></li>
+        </div>
     </ul>
 </nav>
 
